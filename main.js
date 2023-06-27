ddd44abcf1235dcff347c51ae318f26372663e27
Lucas Neno <lucasneno@gmail.com>
2023-06-27 19:56:05 +0100

    Implement random placement of ships for player and ai, improved css display, fix display bug when clicking and selecting entire row

--- main.js
+++ main.js
@@ -23,7 +23,6 @@ let playerHudData;
 let aiHudData;
 let playerShipCounter;
 let aiShipCounter;
-let array;
 
 
 
@@ -32,47 +31,66 @@ let playerBoard = document.querySelector('#playerboard')
 let aiBoard = document.querySelector('#aiboard')
 let playerHud = document.querySelector('#playerhud')
 let aiHud = document.querySelector('#aihud')
+let shipIcon = document.querySelector('.shipIcon')
 
 
 /*----- event listeners -----*/
 document.querySelector('main').addEventListener('click', handlePlayer)
-document.querySelector('main').addEventListener('click', handlePlacement)
+document.querySelector('main').addEventListener('click', randomPlacement)
+//document.querySelector('gamespace').addEventListener('click', select)
+//document.addEventListener('keydown', rotate)
+
+
 // document.querySelector('start').addEventListener('click', startGame)
 
 
 
-/*----- functions -----*/
+/*-------------------------- FUNCTIONS ------------------------------*/
+
+/*--------------------------- GENERAL FUNCTIONS ----------------------*/
 init();
 
 function init() {
-    //generate empty boards and HUDS
+    //Initialize Player
     emptyBoardDisplay(playerBoard);
-    emptyBoardDisplay(aiBoard);
     emptyHudDisplay(playerHud);
-    emptyHudDisplay(aiHud);
     playerBoardData = emptyBoardData(rows, columns);
-    aiBoardData = emptyBoardData(rows, columns);
     playerHudData = newHudData();
+    console.log(playerBoardData);
+    //Initialize AI
+    emptyBoardDisplay(aiBoard);
+    emptyHudDisplay(aiHud);
+    aiBoardData = emptyBoardData(rows, columns);
     aiHudData = newHudData();
-
-
     turn = 1;
     winner = null;
-    render();
+    planningPhase();
 }
 
 console.log(playerBoardData)
-console.log(playerHudData)
-console.log()
+console.log(aiBoardData)
 
-function render() {
+function planningPhase () {
     renderBoardDisplay(playerBoard, playerBoardData);
     renderBoardDisplay(aiBoard, aiBoardData);
-    renderHudDisplay(playerHud);
-    renderHudDisplay(aiHud);
+    renderHudDisplay(playerHud, playerHudData);
+    renderHudDisplay(aiHud, aiHudData);
+    //randomPlacement(playerBoardData);
+    //randomPlacement(aiBoardData); 
+    render()
+}
+
+function render() {
+    handleAi()
+    renderBoardDisplay(playerBoard, playerBoardData);
+    renderBoardDisplay(aiBoard, aiBoardData);
+    renderHudDisplay(playerHud, playerHudData);
+    renderHudDisplay(aiHud, aiHudData);
     renderMessage();
     // renderCounter();
 }
+
+/*------------------------------------------------------------MODEL FUNCTIONS-----------------------------------------------------------------*/
 
 //Create an Empty Board Data - OK!
 function emptyBoardData (rows, columns) {
@@ -83,6 +101,8 @@ function emptyBoardData (rows, columns) {
 function newHudData() {
     return SHIPLIST;
 }
+
+/*------------------------------------------------------------VIEW FUNCTIONS-------------------------------------------------------*/
 
 //Generate Empty Board Display - OK!
 function emptyBoardDisplay(boardEl, columns = 10, rows = 10) {
@@ -110,25 +130,29 @@ function emptyHudDisplay (hudEl) {
         const ship = document.createElement('div');
         if (hudEl === playerHud) {
             ship.setAttribute('id', `p${SHIPLIST[i].name}`);
+            ship.className = SHIPLIST[i].class;
+            ship.className = 'shipIcon';
         } else {
             ship.setAttribute('id', `a${SHIPLIST[i].name}`);
+            ship.className = SHIPLIST[i].class;
+            ship.className = 'shipIcon';
         }
         ship.className = SHIPLIST[i].class;
         hudEl.appendChild(ship);
     }
 }
 
-//UPDATE Board Display using Data for each player
+//UPDATE Board Display using Data for each player - OK!
 function renderBoardDisplay (boardEl, dataEl) {
-    dataEl.forEach(function(rowArr, colIdx){
-        rowArr.forEach(function(cellObj, cellIdx) {
+    dataEl.forEach(function(colArr, colIdx){
+        colArr.forEach(function(cellObj, cellIdx) {
             let cellId;
             if(boardEl === playerBoard) {
                 cellId = `p${cellIdx}${colIdx}`;
             } else {
                 cellId = `a${cellIdx}${colIdx}`;
             }
-            if (cellObj.hit === true) { //check if cellObj is true
+            if (cellObj.hit) { //check if cellObj is true
                 if (cellObj.id === 'w') { 
                     document.getElementById(cellId).style.backgroundColor = 'blue';
                     return;
@@ -140,44 +164,74 @@ function renderBoardDisplay (boardEl, dataEl) {
     });
 }
 
-function renderHudDisplay(){
-
-}
-
-
-//Change display when board is clicked and update ship info in the HUD - OK!
-function handlePlayer (evt) {
-    const tgt = evt.target;
-    //Guards
-    if (tgt.className !== 'cell') return;
-    if (tgt.parentElement.parentElement.id !== 'aiboard') return; //Needs to check 2 parents above
-    // Relate tgt ID to the Board Data 
-    console.log(tgt);
-    const rowId = parseInt(tgt.id.split('').pop()); //returns a number. row id. check:ok
-    const colId = parseInt(tgt.id.split('').slice(1,3)); //return a number. column id. check:ok
-    const cellObj = aiBoardData[rowId][colId]; //assign the position of the element in the 2D array to cellObj variable
-    cellObj.hit = true; // « update board data
-    // UPDATE HUD: if Hit was not water
-    if (cellObj.id !== 'w') {  
-        let shipHit = aiHudData.find((ship) => ship.name === cellObj.id);
-        shipHit.size --; //reduce the ship size in the HUD
-        if(shipHit.size === 0) { //if all of the elements of the corresponding ship have been hit, thus reducind the ship size to 0 in the HudData, then change teh color in the HUD
-            document.getElementById(shipHit.name).style.backgroundColor = 'darkred';
+//UPDATE HUD Display using Data for each player
+function renderHudDisplay(hudEl, dataEl){
+    dataEl.forEach(function(object){
+        if (object.size === 0){
+            if (hudEl === playerHud){
+                document.getElementById(`p${object.name}`).style.backgroundColor = 'darkred';
+            } else {
+                document.getElementById(`a${object.name}`).style.backgroundColor = 'darkred';
+            }
         }
-    }
-    winner = getWinner();
-    turn *= -1;
-    render();
+    })
+
 }
 
+
+/*-----------------------------------////////////////////------CONTROL FUNCTIONS-------------------------------------////////////////------*/
+
+//HANDLE PLAYER: On click, update player board state when the board is clicked and update ship info in the ai HUD - OK!
+function handlePlayer(evt) {
+    if (turn === 1) {
+        const tgt = evt.target;
+        //Guards
+        if (tgt.className !== 'cell') return;
+        if (tgt.parentElement.parentElement.id !== 'aiboard') return; //Needs to check 2 parents above
+        // Relate tgt ID to the Board Data 
+        // console.log(tgt);
+        const rowIdx = parseInt(tgt.id.split('').pop()); //returns a number. column id. check:ok
+        const colIdx = parseInt(tgt.id.split('').slice(1,3)); //return a number. row id. check:ok
+        const cellObj = {...aiBoardData[rowIdx][colIdx], hit:true}; //clone teh object and assign a key-pair of 'hit:true' to it. Store it in 'cellObj' variable.
+        aiBoardData[rowIdx][colIdx] = cellObj; // assign the cellObj value to the desired position
+        //aiBoardData = [...aiBoardData, aiBoardData[rowIdx][colIdx] = cellObj]; <-- NOT WORKING. It replaces the object but also creates an new object at the end of the string
+        console.log(rowIdx, colIdx, cellObj, aiBoardData)
+            // UPDATE HUD: if Hit was not water
+        if (cellObj.id !== 'w') {  
+            let shipHit = aiHudData.find((ship) => ship.name === cellObj.id);
+            shipHit.size --; //reduce the ship size in the HUD
+            if(shipHit.size === 0) { //if all of the elements of the corresponding ship have been hit, thus reducind the ship size to 0 in the HudData, then change teh color in the HUD
+                document.getElementById(shipHit.name).style.backgroundColor = 'darkred';
+            }
+        }
+        winner = getWinner();
+        turn *= -1;
+        render();
+    } return;
+}
+
+//HANDLE AI: Basic AI randomized move. Update ai board state and player HUD. NO LOGIC IMPLEMENTED
 function handleAi () {
     if (turn === -1) {
-
-    }
+        let rowIdx = Math.floor(Math.random()*10);
+        let colIdx = Math.floor(Math.random()*10);
+        const cellObj = {...playerBoardData[rowIdx][colIdx], hit:true}; //clone teh object and assign a key-pair of 'hit:true' to it. Store it in 'cellObj' variable.
+        playerBoardData[rowIdx][colIdx] = cellObj; // assign the cellObj value to the desired position
+        if (cellObj.id !== 'w') {  
+            let shipHit = playerHudData.find((ship) => ship.name === cellObj.id);
+            shipHit.size --; //reduce the ship size in the HUD
+            if(shipHit.size === 0) { //if all of the elements of the corresponding ship have been hit, thus reducind the ship size to 0 in the HudData, then change teh color in the HUD
+                document.getElementById(shipHit.name).style.backgroundColor = 'darkred';
+            }
+        }
+        winner = getWinner();
+        turn *= -1;
+        render();
+    } return;
     //check DOM state: iterate through array; if there is a hit === true && go to random location; click; 
 }
 
-// Check Winning Conditon and return if someon won;
+//CHECK WINNER: Check Winning Conditon and return if someone won; - OK!
 function getWinner() {
     const playerRemainingShips = playerHudData.reduce((acc,ship) => {
         return acc + ship.size;
@@ -190,21 +244,86 @@ function getWinner() {
 
 }
 
+//RANDOM SHIP PLACEMENT - OK!
+function randomPlacement(boardEl){
+//1) iterate through ship array and do this for each ship: 
+    SHIPLIST.forEach(function(ship){
+        let success = false;
+        while(!success){
+            //2.1) pick a random number coordinates, ranging from 0 to (row or column) and place on the board
+            let rowIdx = Math.floor(Math.random()*10);
+            let colIdx = Math.floor(Math.random()*10);
+            //2.2) pick random orientation (1 or 0). FUTURE: make it so that it can be 3 or 4 for ship orientation
+            let numRotations = Math.floor(Math.random()*2);
+            //2.3) check if squares are available (use coordinates, rotation and check if it exists && {id:'w'})
+            if(checkPlacement (boardEl, rowIdx, colIdx, ship, numRotations)){
+                //2.4) if yay: replace data; if nay: go back to 2.1
+                placeShip(boardEl, rowIdx, colIdx, ship, numRotations)
+                success = true;
+            };
+        }
+    });
+    //3) appear start button 
+};
+            
+//CHECK PLACEMENT: Check if there are free squares within range of the selected coordinates for the selected ship - OK!
+function checkPlacement(boardEl, rowIdx, colIdx, ship, direction) {
+    let length = ship.size
+    if (direction === 0) {
+        if (colIdx+length <= columns) {
+            for (let i=0; i<length; i++) {
+                if(boardEl[rowIdx][colIdx+i].id === 'w') { 
+                  return true;
+                }
+            };
+        } return false;
+    } else if (rowIdx+length <= rows) {
+            for (let i=0; i<length; i++) {
+                if(boardEl[rowIdx+i][colIdx].id === 'w') {
+                  return true;
+                }
+            }            
+        } 
+  return false;
+}
 
-//HUD Update
-// function renderCounter () {
-//     aiboardShipCounter = SHIPLIST;
-//     console.log(SHIPLIST);
-//     aiShipCounter.forEach((ship) => {
-//         aiboardBoardData.forEach((row) => {
-//             row.forEach((cell) => {
-//                 if (cell.hit === true && cell.id === ship.id) {
-//                     ship.size --;
-//                 } return;
-//             });
-//         });
-//     });
+
+// function checkPlacement(boardEl, rowIdx, colIdx, ship, direction) {
+//     let length = ship.size
+//     if (direction === 0) {
+//         if (colIdx+length > columns) return false;
+//         for (let i=0; i<length; i++) {
+//             if(boardEl[rowIdx][colIdx+i].id !== 'w') return false;
+//         }
+//     } else {
+//         if (rowIdx+length > rows) return false;
+//         for (let i=0; i<length; i++) {
+//             if(boardEl[rowIdx+i][colIdx].id !== 'w') return false;
+//         }
+//     }
+//     return true;
 // }
+
+
+
+
+//PLACE SHIP: Place the ship on the board
+function placeShip (boardEl, rowIdx, colIdx, ship, direction) {
+    let length = ship.size
+    if (direction === 0) {
+        for (let i=0; i<length; i++) {
+            let clone = {...boardEl[rowIdx][colIdx], id:ship.name}; //create clone replacing the cell 'id' value
+            boardEl[rowIdx][colIdx] = clone; // assign the value in the board to be a clone
+            colIdx++;
+        }
+    } else {
+        for (let i=0; i<length; i++) {
+            let clone = {...boardEl[rowIdx][colIdx], id:ship.name}; //create clone replacing the cell 'id' value
+            boardEl[rowIdx][colIdx] = clone; // assign the value in the board to be a clone
+            rowIdx++;           
+        }
+    }
+}
 
 function renderMessage() {
 
@@ -214,34 +333,26 @@ function startGame(evt) {
 
 }
 
-function handlePlacement(evt) {
-//     //Guards
-//     if (tgt.className !== 'row') return;
-//     if (tgt.parentElement.parentElement.id !== 'shipcounter') return;
-//     //
-}
+/*-----------------------UNUSED CODE ----------------------------------*/
 
+// generate an array with the ship properties - OK! (UNUSED)
+// function generateShipArray(ship) {
+//     let size = ship.size;
+//     let name = ship.name;
+//     return array = Array(1).fill().map(function() {
+//         return Array(size).fill().map(function() {
+//             return {id:name}
+//         });
+//     });
+// }
 
-
-    //Generate Ships
-    // for (let i=0; i<2; i++) //make this 2 times
-    // const shipIcons = document.createElement('div');
-    // shipIcons.className = 'ship';
-    
-
-
-function shipPlacement () {
-
-}
-
-
-/*let drag = false;
-document.addEventListener(
-    'mousedown', () => drag = false);
-  
-document.addEventListener(
-    'mousemove', () => drag = true);
-  
-document.addEventListener(
-    'mouseup', () => console.log(
-        drag ? 'drag' : 'click'));*/
+// ROTATE SHIP in data - OK! (UNUSED)
+// function rotate(array) {
+//     let rotate =[];
+//     array.forEach(function (subArray) {
+//     subArray.forEach(function(cell, idx) {
+//         rotate[idx] = [...(rotate[idx] || []), cell]
+//     });
+//     });
+//     return rotate;
+// }
